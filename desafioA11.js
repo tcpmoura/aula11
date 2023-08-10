9cd68f0eaae705d701ddc320f44ac89aaef43736
Tatiana Caetano Praxedes de Moura <137582003+tcpmoura@users.noreply.github.com>
2023-08-10 15:50:54 -0300

    Add files via upload

---
+++ desafioA11.js
@@ -0,0 +1,6 @@
+var num = parseFloat(prompt('Digite um número:'));
+    if(num%2 === 0 && num>0 && num<100){
+      alert('O número atende aos critérios.');
+    }else{
+      alert('O número não atende aos critérios.');
+    }
